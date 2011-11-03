631c77fa748e20b38208841e155360514461d129
Will Lee <vclwei@localhost.(none)>
2011-11-03 23:43:46 +0800

    Initial Commit

---
+++ CengMe/CMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMAppDelegate.h
+//  CengMe
+//
+//  Created by Will Lee on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CengMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CengMe
+//
+//  Created by Will Lee on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMAppDelegate class]));
+    }
+}
